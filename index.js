d0397476f91986bfca4fd4a9ee05ce806cb3cf6e
Adetoye Babalola <adetoyebamise@gmail.com>
2022-01-27 00:20:06 +0100

    solution one and partial infomat

--- index.js
+++ index.js
@@ -22,6 +22,37 @@ More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
 
 */
 
+// Solution 1
+const solution = (value, sign="") => {
+    if (value >= 1000) {
+      sign += "M"        //concat. assignment
+      value -= 1000      //subtr. assignment
+    } else if (value >= 900) {
+      sign += "CM"
+      value -= 900
+    } else if(value >= 600) {
+      sign += "DC"
+      value -= 600
+    } else if(value >= 50) {
+      sign += "L"
+      value -= 50
+    }
+    // ... create for every condition 
+    return value ? solution(value, sign) : sign
+  }
+  
+  //call function and log
+  console.log(solution(700, "The answer is: "))
+
+
+// solution 2
+const solution = (value) => {
+    const roman = { M : 1000, CM: 900, DCCC : 800, DCC : 700, DC: 600, D : 500, CD : 400, CCC : 300, CC : 200, C : 100, XC : 90, CXXX: 80,  CXX :70, CX : 60, L: 50, XL: 40, XXX: 30, XX: 20, X:10, IX:9, XVIII: 8, VII:7, V1 : 6, V : 5, IV : 4, III: 3, II: 2, I: 1 }
+
+    
+}
+
+// solution 3
 const romanNumeralDecoder = (number) => {
 
 
