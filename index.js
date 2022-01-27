96579390f417a76286d4678d61900f442af84ef4
Adetoye Babalola <adetoyebamise@gmail.com>
2022-01-27 12:50:56 +0100

    solution(3)

--- index.js
+++ index.js
@@ -46,14 +46,35 @@ const solution = (value, sign="") => {
 
 
 // solution 2
-const solution = (value) => {
+const solution = (number) => {
     const roman = { M : 1000, CM: 900, DCCC : 800, DCC : 700, DC: 600, D : 500, CD : 400, CCC : 300, CC : 200, C : 100, XC : 90, CXXX: 80,  CXX :70, CX : 60, L: 50, XL: 40, XXX: 30, XX: 20, X:10, IX:9, XVIII: 8, VII:7, V1 : 6, V : 5, IV : 4, III: 3, II: 2, I: 1 }
-
+    // Object.Keys() return an array containing keys of the Object - roman
+    const value = "",
     
+    for (const iterator of Object.keys(roman)) {
+        const quest = Math.floor( number / roman[iterator])
+
+        number -= quest * roman[iterator]      // roman[index] access the property of the object
+        value += iterator.repeat(quest)
+    }
+    return value
 }
+
+solution(4852)
 
 // solution 3
 const romanNumeralDecoder = (number) => {
+  result = ''
+  decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
 
-
+  roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  decimals.map((value, index) => {
+    while (number >= value) {                    // this condition abstracts the else ifs' condition
+    number -= value
+    result += roman[index]
+    }
+  })
+  return result
 }
+
+romanNumeralDecoder(3564)
