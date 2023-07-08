57b6fae8ea0dfff72db2ab9efe87615ee5b4db90
Adetoye Babalola <adetoyebamise@gmail.com>
2023-07-08 17:12:56 -0400

    regex password validation

--- regexpasswordvalidation.js
+++ regexpasswordvalidation.js
@@ -8,10 +8,14 @@
 // only contains alphanumeric characters (note that '_' is not alphanumeric)
 
 function validate(password) {
-  const Upper = password.match(/[A-Z]/);
-  const Lower = password.match(/[a-z]/);
-  const Number = password.match(/[0-9]/);
-  const Length = password.length > 5;
-  if (password.replace(/[0-9A-Z]/gi, "").length > 0) return false;
-  return Upper && Lower && Number && Length;
+  const Upper = password.match(/[A-Z]/)
+  const Lower = password.match(/[a-z]/)
+  const Number = password.match(/[0-9]/)
+  const Length = password.length > 5
+  if (password.replace(/[0-9A-Z]/gi, "").length > 0) return false
+  return Upper && Lower && Number && Length
+}
+
+function validate(password) {
+  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/.test(password)
 }
